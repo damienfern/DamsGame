350881908e7e110e2d4816e0f7cecc9fb7af9984
damienfern <damien.fernandes24@gmail.com>
2018-03-28 14:30:12 +0200

    Exo 1 fini

--- CheckersGameNotPOO.java
+++ CheckersGameNotPOO.java
@@ -38,16 +38,53 @@ public class CheckersGameNotPOO {
                 if ((i+j) % 2 == 0)
                 {
                     panel.setBackground(Color.BLACK);
+                    if (i < 3 || i > 6)
+                    {
+                        JCircle pion;
+                        if(i>6)
+                        {
+                            pion = new JCircle(Color.RED);
+                        }
+                        else
+                        {
+                            pion = new JCircle(new Color(173, 0, 255));
+                        }
+                        pion.setPreferredSize(new Dimension(50, 50));
+                        pion.setOpaque(false);
+                        panel.add(pion);
+                    }
                 }
                 else
                 {
+
                     panel.setBackground(Color.WHITE);
                 }
+
+                panel.setAlignmentX(JComponent.CENTER_ALIGNMENT);
+                panel.setAlignmentY(JComponent.CENTER_ALIGNMENT);
                 frame.add(panel);
             }
 
         }
 
         return frame;
+    }
+
+    private class JCircle extends JPanel
+    {
+        protected Color couleur;
+
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+
+            g.setColor(couleur);
+            g.fillOval(0,0, getHeight(),getWidth());
+        }
+
+        public JCircle(Color couleur) {
+            this.couleur = couleur;
+        }
     }
 }
