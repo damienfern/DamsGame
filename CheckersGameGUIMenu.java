9bd99e01ba3893f96c6b521ff9c5f4218526ea14
damienfern <damien.fernandes24@gmail.com>
2018-04-10 14:12:37 +0200

    7.b -> Ajout listener sur menu + recup color JCOLORCHOOSER

--- CheckersGameGUIMenu.java
+++ CheckersGameGUIMenu.java
@@ -1,16 +1,37 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CheckersGameGUIMenu extends JMenuBar {
 
     private JMenu choix_couleur_menu = new JMenu("Choix couleur");
 
-
+    private CheckersGameGUIData checkersGameGUIData;
     private JMenuItem choix_couleur_pblanc = new JMenuItem("Piece Blanche");
     private JMenuItem choix_couleur_pnoire = new JMenuItem("Piece Noire");
 
-    public CheckersGameGUIMenu(CheckersGameGUIData checkersGameGUIData) {
+    public CheckersGameGUIMenu(final CheckersGameGUIData checkersGameGUIData)
+    {
+        this.checkersGameGUIData = checkersGameGUIData;
         choix_couleur_menu.add(choix_couleur_pblanc);
         choix_couleur_menu.add(choix_couleur_pnoire);
+
+        choix_couleur_pblanc.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                Color new_couleur = JColorChooser.showDialog(choix_couleur_pblanc, "Sélectionner une couleur pour les pièces blanches", checkersGameGUIData.getColorWhitePiece());
+                System.out.println(new_couleur.toString());
+            }
+        });
+
+        choix_couleur_pnoire.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                Color new_couleur = JColorChooser.showDialog(choix_couleur_pnoire, "Sélectionner une couleur pour les pièces noires", checkersGameGUIData.getColorBlackPiece());
+            }
+        });
+
         this.add(choix_couleur_menu);
     }
 }
