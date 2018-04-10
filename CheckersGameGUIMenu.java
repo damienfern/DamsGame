283ec6319e18383a359dbf401cd99fca07dcddf0
damienfern <damien.fernandes24@gmail.com>
2018-04-10 13:57:52 +0200

    7.a -> Creation menu

--- CheckersGameGUIMenu.java
+++ CheckersGameGUIMenu.java
@@ -1,6 +1,16 @@
 import javax.swing.*;
 
 public class CheckersGameGUIMenu extends JMenuBar {
+
+    private JMenu choix_couleur_menu = new JMenu("Choix couleur");
+
+
+    private JMenuItem choix_couleur_pblanc = new JMenuItem("Piece Blanche");
+    private JMenuItem choix_couleur_pnoire = new JMenuItem("Piece Noire");
+
     public CheckersGameGUIMenu(CheckersGameGUIData checkersGameGUIData) {
+        choix_couleur_menu.add(choix_couleur_pblanc);
+        choix_couleur_menu.add(choix_couleur_pnoire);
+        this.add(choix_couleur_menu);
     }
 }
